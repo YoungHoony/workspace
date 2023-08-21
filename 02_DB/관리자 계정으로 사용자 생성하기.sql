d03e56e3ce10a378a04d474349f6b2032e2d8976
David <yhoon_0802@hotmail.com>
2023-08-21 10:39:35 +0900

    230821_ 사용자 계정 생성

---
+++ 02_DB/관리자 계정으로 사용자 생성하기.sql
@@ -0,0 +1,16 @@
+-- 한 줄 주석 ctrl +/
+/* 범위 주석 ctrl + shift + / */
+
+-- SQL 한 줄 실행 : ctrl + enter
+
+-- 오라클에서는 " "(쌍따옴표)의 의미 : 대문자/소문자를 구문하겠다. 
+
+-- _ORACLE_SCRIPT 예전 버전 (11g 이전 버전) 오라클 구문 사용하기
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+CREATE USER a230724_jyh IDENTIFIED BY "Password1!";
+
+GRANT CONNECT, RESOURCE, CREATE VIEW TO a230724_jyh;
+
+ALTER USER a230724_jyh DEFAULT TABLESPACE "A230724"
+QUOTA 20M ON "A230724";
