74502c006e80ba2fd512459571897cc75237122b
David <yhoon_0802@hotmail.com>
2023-10-19 15:02:49 +0900

    231019 admin page

--- 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/controller/AdminController.java
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/controller/AdminController.java
@@ -1,13 +1,18 @@
 package edu.kh.project.admin.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.project.admin.model.service.AdminService;
 import edu.kh.project.member.model.dto.Member;
@@ -15,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
-@SessionAttributes({"loginMember"})
 @RequestMapping("admin") // admin이라는 주소에서 나오는 모든 걸 다 처리한다
 public class AdminController {
 	
@@ -55,4 +59,93 @@ public class AdminController {
 		return "admin/fail";
 	}
 
+	/** 회원 전체 조회
+	 * @param model : 데이터 전달하는 역할
+	 * @return "admin/selectAll"
+	 */
+	@GetMapping("selectAll")
+	public String selectAll(Model model) {
+		
+		// Collection : Java 자료구조 모음(List, Set, Map)
+		// service.selectAll 이라는 것을 호출
+		List<Member> memberList = service.selectAll();
+		
+		// 조회 결과를 request scope로 전달
+		model.addAttribute("memberList", memberList);
+				
+		return "admin/selectAll";
+		
+	}
+	
+	/** 회원 전체 조회 (+정렬)
+	 * @param model : 데이터 전달 객체 
+	 * @return "admin/selectSort"
+	 */
+	@GetMapping("selectSort")
+	public String selectSort(Model model, 
+			@RequestParam(value="sort", required = false, defaultValue ="1") int sort) {
+		
+		List<Member> memberList = service.selectSort(sort);
+		
+		model.addAttribute("memberList", memberList);
+		
+		return "admin/selectSort";
+	}
+	
+	/** 회원 복구
+	 * @param memberNo : 회원 번호
+	 * @param memberEmail : 회원 이메일
+	 * @param ra : 리다이렉트 시 데이터 전달
+	 * @return
+	 */
+	@PostMapping("restoration")
+	public String restoration( int memberNo, String memberEmail, RedirectAttributes ra) {
+		
+		// 복구 서비스 호출 (memberNo을 가공해서 넘겨주기)
+		int result = service.restoration(memberNo);
+		
+		if(result > 0) {
+			ra.addFlashAttribute("message", "복구 성공");
+		}  else {
+			ra.addFlashAttribute("message", "복구 실패");
+		}
+		
+		// selectMember?inputEmail=test06@kh.com
+		return "redirect:selectMember?inputEmail=" + memberEmail;
+	}
+	
+	@PostMapping("changeAuthority")
+	public String changeAuthority(int memberNo, String memberEmail, RedirectAttributes ra) {
+		
+		int result = service.changeAuthority(memberNo);
+		
+		if(result> 0) {
+			ra.addFlashAttribute("message", "회원 권한 변경 성공");
+		} else {
+			ra.addFlashAttribute("message", "회원 권한 변경 실패");
+		}
+		
+		return "redirect:selectMember?inputEmail=" + memberEmail;
+		
+	}
+	
+	/** 비밀번호를 "1234" (암호화 진행!)로 초기화
+	 * @param memberNo
+	 * @return
+	 */
+	@PostMapping("initPw")
+	public String initPw(String memberEmail, RedirectAttributes ra, int memberNo ) {
+		
+		int result = service.initPw(memberNo);
+		
+		if(result>0) {
+			ra.addFlashAttribute("message", "비밀번호 초기화 완료");
+		} else {
+			ra.addFlashAttribute("message", "비밀번호 초기화 실패");
+		}
+		
+		return "redirect:selectMember?inputEmail=" + memberEmail;
+	}
+	
+	
 }
--- 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/model/service/AdminServiceImpl.java
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/model/service/AdminServiceImpl.java
@@ -1,6 +1,11 @@
 package edu.kh.project.admin.model.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,10 +19,66 @@ public class AdminServiceImpl implements AdminService {
 	@Autowired
 	private AdminMapper mapper;
 	
-@Override
-public Member selectMember(String inputEmail) {
+	@Autowired //암호화 객체 의존성 주입
+	private BCryptPasswordEncoder bcrypt;
 	
-	return mapper.selectMember(inputEmail);
-}
+	@Override
+	public Member selectMember(String inputEmail) {
+		
+		return mapper.selectMember(inputEmail);
+	}
 	
+	
+	@Override
+	public List<Member> selectAll() {
+		return mapper.selectAll();
+		// 가공할 것이 없어, 아무것도 적지 않음. 
+	}
+		
+	@Override
+	public List<Member> selectSort(int sort) {
+		
+		// sort 값에 따라 가공 처리
+		String query = null;
+		
+		switch(sort) {
+		case 1: query = "MEMBER_NO DESC"; break; // 회원번호 내림차순
+		case 2: query = "MEMBER_NO"; break; // 회원번호 오름차순
+		case 3: query = "MEMBER_EMAIL DESC"; break; // 이메일 내림차순
+		case 4: query = "MEMBER_EMAIL"; break; // 이메일 오름차순
+		}
+		
+		
+		return mapper.selectSort(query);
+	}
+	
+	
+	@Override
+	public int restoration(int memberNo) {
+		return mapper.restoration(memberNo);
+	}
+	
+	
+	@Override
+	public int changeAuthority(int memberNo) {
+		return mapper.changeAuthority(memberNo);
+	}
+	
+	@Override
+	public int initPw(int memberNo) {
+		
+		// 암호화된 1234 만들기
+		String encPw = bcrypt.encode("1234");
+		
+		// 마이바티스 코드에서 파라미터는 1개만 가능
+		Map<String, Object> map = new HashMap<>();
+		map.put("encPw", encPw);
+		map.put("memberNo", memberNo);
+		
+		return mapper.initPw(map);
+	}
+
+
+
+
 }
